990ad27d951c8267f848e4cfce5254e7a77ce6bd
Xavier Flix <xavier@xfx.net>
2016-12-29 01:03:45 -0400

    Fixed resizing issues with the UI

--- LSysInt Tester/FormMain.Designer.vb
+++ LSysInt Tester/FormMain.Designer.vb
@@ -23,7 +23,6 @@ Partial Class FormMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.SplitContainerMain = New System.Windows.Forms.SplitContainer()
-        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.TextBoxCode = New System.Windows.Forms.TextBox()
         Me.ComboBoxLSysCodeDefs = New System.Windows.Forms.ComboBox()
         Me.LSysRendererCtrl = New LSysInt.LSysRenderer()
@@ -31,7 +30,6 @@ Partial Class FormMain
         Me.SplitContainerMain.Panel1.SuspendLayout()
         Me.SplitContainerMain.Panel2.SuspendLayout()
         Me.SplitContainerMain.SuspendLayout()
-        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'SplitContainerMain
@@ -42,56 +40,47 @@ Partial Class FormMain
         '
         'SplitContainerMain.Panel1
         '
-        Me.SplitContainerMain.Panel1.Controls.Add(Me.TableLayoutPanel1)
+        Me.SplitContainerMain.Panel1.Controls.Add(Me.ComboBoxLSysCodeDefs)
+        Me.SplitContainerMain.Panel1.Controls.Add(Me.TextBoxCode)
         '
         'SplitContainerMain.Panel2
         '
         Me.SplitContainerMain.Panel2.Controls.Add(Me.LSysRendererCtrl)
-        Me.SplitContainerMain.Size = New System.Drawing.Size(1365, 716)
-        Me.SplitContainerMain.SplitterDistance = 455
+        Me.SplitContainerMain.Size = New System.Drawing.Size(951, 567)
+        Me.SplitContainerMain.SplitterDistance = 303
         Me.SplitContainerMain.TabIndex = 1
-        '
-        'TableLayoutPanel1
-        '
-        Me.TableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
-        Me.TableLayoutPanel1.ColumnCount = 1
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.TableLayoutPanel1.Controls.Add(Me.TextBoxCode, 0, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.ComboBoxLSysCodeDefs, 0, 0)
-        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 2
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(455, 716)
-        Me.TableLayoutPanel1.TabIndex = 1
         '
         'TextBoxCode
         '
+        Me.TextBoxCode.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBoxCode.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
         Me.TextBoxCode.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TextBoxCode.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TextBoxCode.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBoxCode.ForeColor = System.Drawing.Color.GreenYellow
-        Me.TextBoxCode.Location = New System.Drawing.Point(3, 32)
+        Me.TextBoxCode.Location = New System.Drawing.Point(0, 23)
+        Me.TextBoxCode.Margin = New System.Windows.Forms.Padding(0)
         Me.TextBoxCode.Multiline = True
         Me.TextBoxCode.Name = "TextBoxCode"
-        Me.TextBoxCode.Size = New System.Drawing.Size(449, 681)
+        Me.TextBoxCode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBoxCode.Size = New System.Drawing.Size(303, 544)
         Me.TextBoxCode.TabIndex = 0
         '
         'ComboBoxLSysCodeDefs
         '
+        Me.ComboBoxLSysCodeDefs.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ComboBoxLSysCodeDefs.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
-        Me.ComboBoxLSysCodeDefs.Dock = System.Windows.Forms.DockStyle.Top
         Me.ComboBoxLSysCodeDefs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBoxLSysCodeDefs.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ComboBoxLSysCodeDefs.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ComboBoxLSysCodeDefs.ForeColor = System.Drawing.Color.WhiteSmoke
         Me.ComboBoxLSysCodeDefs.FormattingEnabled = True
-        Me.ComboBoxLSysCodeDefs.Location = New System.Drawing.Point(3, 3)
+        Me.ComboBoxLSysCodeDefs.Location = New System.Drawing.Point(1, 0)
+        Me.ComboBoxLSysCodeDefs.Margin = New System.Windows.Forms.Padding(0)
         Me.ComboBoxLSysCodeDefs.Name = "ComboBoxLSysCodeDefs"
-        Me.ComboBoxLSysCodeDefs.Size = New System.Drawing.Size(449, 23)
+        Me.ComboBoxLSysCodeDefs.Size = New System.Drawing.Size(302, 23)
         Me.ComboBoxLSysCodeDefs.TabIndex = 1
         '
         'LSysRendererCtrl
@@ -100,7 +89,7 @@ Partial Class FormMain
         Me.LSysRendererCtrl.LDef = Nothing
         Me.LSysRendererCtrl.Location = New System.Drawing.Point(0, 0)
         Me.LSysRendererCtrl.Name = "LSysRendererCtrl"
-        Me.LSysRendererCtrl.Size = New System.Drawing.Size(906, 716)
+        Me.LSysRendererCtrl.Size = New System.Drawing.Size(644, 567)
         Me.LSysRendererCtrl.TabIndex = 0
         '
         'FormMain
@@ -108,7 +97,7 @@ Partial Class FormMain
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DimGray
-        Me.ClientSize = New System.Drawing.Size(1365, 716)
+        Me.ClientSize = New System.Drawing.Size(951, 567)
         Me.Controls.Add(Me.SplitContainerMain)
         Me.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "FormMain"
@@ -116,11 +105,10 @@ Partial Class FormMain
         Me.Text = "LSysInt Tester"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.SplitContainerMain.Panel1.ResumeLayout(False)
+        Me.SplitContainerMain.Panel1.PerformLayout()
         Me.SplitContainerMain.Panel2.ResumeLayout(False)
         CType(Me.SplitContainerMain, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainerMain.ResumeLayout(False)
-        Me.TableLayoutPanel1.ResumeLayout(False)
-        Me.TableLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -128,6 +116,5 @@ Partial Class FormMain
     Friend WithEvents LSysRendererCtrl As LSysInt.LSysRenderer
     Friend WithEvents SplitContainerMain As SplitContainer
     Friend WithEvents TextBoxCode As TextBox
-    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents ComboBoxLSysCodeDefs As ComboBox
 End Class
--- LSysInt/My Project/AssemblyInfo.vb
+++ LSysInt/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2016.12.29.131")>
-<Assembly: AssemblyFileVersion("2016.12.29.131")>
+<Assembly: AssemblyVersion("2016.12.29.132")>
+<Assembly: AssemblyFileVersion("2016.12.29.132")>
